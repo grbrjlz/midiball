6d8782203f7f73dd8cec98eb6d8c573b3fa3db22
Julian Graeber <julian.graeber@student.hs-rm.de>
2022-01-14 21:18:37 +0100

    Bendsensor und Gyrosensor

--- midiball-v1/midiball-v1.ino
+++ midiball-v1/midiball-v1.ino
@@ -1,12 +1,33 @@
+#include <Wire.h>
 #include <Controlino.h>
-#include <Midier.h>
 
-//midier::Layers<8> layers;
-//midier::Sequencer sequencer(layers);
+const int MPU_ADDR = 0x68; // I2C address of the MPU-6050. If AD0 pin is set to HIGH, the I2C address will be 0x69.
+int16_t accelerometer_x, accelerometer_y, accelerometer_z; // variables for accelerometer raw data
+int16_t gyro_x, gyro_y, gyro_z; // variables for gyro raw data
+int16_t temperature; // variables for temperature data
+char tmp_str[7]; // temporary variable used in convert function
+
+const int FLEX_PIN = A0; // Pin connected to voltage divider output
+// Measure the voltage at 5V and the actual resistance of your
+// 47k resistor, and enter them below:
+const float VCC = 4.98; // Measured voltage of Ardunio 5V line
+const float R_DIV = 47500.0; // Measured resistance of 3.3k resistor
+
+// Upload the code, then try to adjust these values to more
+// accurately calculate bend degree.
+const float STRAIGHT_RESISTANCE = 37300.0; // resistance when straight
+const float BEND_RESISTANCE = 90000.0; // resistance at 90 deg
+
 const int buttonPin = 2;     // the number of the pushbutton pin
 // variables will change:
 int buttonState = 0;         // variable for reading the pushbutton status
 boolean isPressed = false;
+
+char* convert_int16_to_str(int16_t i) { // converts int16 to string. Moreover, resulting strings will have the same length in the debug monitor.
+  sprintf(tmp_str, "%6d", i);
+  return tmp_str;
+}
+
 
 void setup() {
   // initialize the LED pin as an output:
@@ -14,6 +35,14 @@ void setup() {
   Serial.begin(9600);
   // initialize the pushbutton pin as an input:
   pinMode(buttonPin, INPUT);
+  pinMode(FLEX_PIN, INPUT);
+  
+  Wire.begin();
+  Wire.beginTransmission(MPU_ADDR); // Begins a transmission to the I2C slave (GY-521 board)
+  Wire.write(0x6B); // PWR_MGMT_1 register
+  Wire.write(0); // set to zero (wakes up the MPU-6050)
+  Wire.endTransmission(true);
+  
 }
 
 
@@ -33,4 +62,47 @@ void loop() {
     }
     isPressed = false;
   }
+  
+  Wire.beginTransmission(MPU_ADDR);
+  Wire.write(0x3B); // starting with register 0x3B (ACCEL_XOUT_H) [MPU-6000 and MPU-6050 Register Map and Descriptions Revision 4.2, p.40]
+  Wire.endTransmission(false); // the parameter indicates that the Arduino will send a restart. As a result, the connection is kept active.
+  Wire.requestFrom(MPU_ADDR, 7*2, true); // request a total of 7*2=14 registers
+  
+  // "Wire.read()<<8 | Wire.read();" means two registers are read and stored in the same variable
+  accelerometer_x = Wire.read()<<8 | Wire.read(); // reading registers: 0x3B (ACCEL_XOUT_H) and 0x3C (ACCEL_XOUT_L)
+  accelerometer_y = Wire.read()<<8 | Wire.read(); // reading registers: 0x3D (ACCEL_YOUT_H) and 0x3E (ACCEL_YOUT_L)
+  accelerometer_z = Wire.read()<<8 | Wire.read(); // reading registers: 0x3F (ACCEL_ZOUT_H) and 0x40 (ACCEL_ZOUT_L)
+  //temperature = Wire.read()<<8 | Wire.read(); // reading registers: 0x41 (TEMP_OUT_H) and 0x42 (TEMP_OUT_L)
+  gyro_x = Wire.read()<<8 | Wire.read(); // reading registers: 0x43 (GYRO_XOUT_H) and 0x44 (GYRO_XOUT_L)
+  gyro_y = Wire.read()<<8 | Wire.read(); // reading registers: 0x45 (GYRO_YOUT_H) and 0x46 (GYRO_YOUT_L)
+  gyro_z = Wire.read()<<8 | Wire.read(); // reading registers: 0x47 (GYRO_ZOUT_H) and 0x48 (GYRO_ZOUT_L)
+  
+  // print out data
+  Serial.print("aX = "); Serial.print(convert_int16_to_str(accelerometer_x));
+  Serial.print(" | aY = "); Serial.print(convert_int16_to_str(accelerometer_y));
+  Serial.print(" | aZ = "); Serial.print(convert_int16_to_str(accelerometer_z));
+  // the following equation was taken from the documentation [MPU-6000/MPU-6050 Register Map and Description, p.30]
+  //Serial.print(" | tmp = "); Serial.print(temperature/340.00+36.53);
+  Serial.print(" | gX = "); Serial.print(convert_int16_to_str(gyro_x));
+  Serial.print(" | gY = "); Serial.print(convert_int16_to_str(gyro_y));
+  Serial.print(" | gZ = "); Serial.print(convert_int16_to_str(gyro_z));
+  Serial.println();
+
+  // Read the ADC, and calculate voltage and resistance from it
+  /*int flexADC = analogRead(FLEX_PIN);
+  float flexV = flexADC * VCC / 1023.0;
+  float flexR = R_DIV * (VCC / flexV - 1.0);
+  Serial.println("Resistance: " + String(flexR) + " ohms");
+
+  // Use the calculated resistance to estimate the sensor's
+  // bend angle:
+  float angle = map(flexR, STRAIGHT_RESISTANCE, BEND_RESISTANCE,
+                   0, 90.0);
+  Serial.println("Bend: " + String(angle) + " degrees");
+  Serial.println();
+  */
+  
+  // delay
+  delay(1000);
+  
 }
--- serialmidi-master/test.py
+++ serialmidi-master/test.py
@@ -5,29 +5,28 @@ import serial
 
 movie1 = "/home/pi/Videos/test.mp4"
 
-ser = serial.Serial('/dev/cu.usbmodem141201',9600)
+ser = serial.Serial('/dev/cu.usbmodem141101',9600)
 midiout = rtmidi.MidiOut()
 available_ports = midiout.get_ports()
 print(available_ports)
-
 while True:
     command = ser.read()
     sig_value = command[-1] - 48
     if command:
         print(sig_value)
         if sig_value == 0:
+            print("value gleich null")
             if available_ports:
                 midiout.open_port(0)
-        else:
-            midiout.open_virtual_port("My virtual output")
-
-        with midiout:
-            note_on = [0x90, 60, 112] # channel 1, middle C, velocity 112
-            note_off = [0x80, 60, 0]
-            midiout.send_message(note_on)
-            time.sleep(0.5)
-            midiout.send_message(note_off)
-            time.sleep(0.1)
+            else:
+                midiout.open_virtual_port("My virtual output")
+            with midiout:
+                note_on = [0x90, 60, 112] # channel 1, middle C, velocity 112
+                note_off = [0x80, 60, 0]
+                midiout.send_message(note_on)
+                time.sleep(0.5)
+                midiout.send_message(note_off)
+                time.sleep(0.1)
 
 del midiout        
         # flush serial for unprocessed data
